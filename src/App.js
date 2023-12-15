0c5546d451e3683debb7fc65e6540c5829a3054a
femmytedrey <femmytedrey@gmail.com>
2023-12-15 23:18:58 +0100

    rendering

--- src/App.js
+++ src/App.js
@@ -4,7 +4,35 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchJobs()
+  }, [])
+
+  if(loading){
+    return <section className='section loading'>
+      <h1>loading...</h1>
+    </section>
+  }
+  const { company, dates, duties, title } = jobs[value]
+  // console.log(jobs)
+
+  return <section className='section'>
+    <div className='title'>
+      <h2>experience</h2>
+      <div className='underline'></div>
+    </div>
+  </section>
 }
 
 export default App
